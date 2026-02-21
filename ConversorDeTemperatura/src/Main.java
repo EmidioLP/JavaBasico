184fe7de95a617a111839d1bcdd8b5eaf7c7f486
Emidio Lopes <37850911+EmidioLP@users.noreply.github.com>
2026-02-21 15:15:12 -0300

    Novo projeto adicionado

--- ConversorDeTemperatura/src/Main.java
+++ ConversorDeTemperatura/src/Main.java
@@ -1,6 +1,25 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args){
 
+        Scanner scanner = new Scanner(System.in);
+
+        double temp;
+        double novaTemp;
+        String unidade;
+
+        System.out.print("Adicione a temperatura: ");
+        temp = scanner.nextDouble();
+
+        System.out.print("Converter para Celsius ou Fahrenheit? (C ou F): " );
+        unidade = scanner.next().toUpperCase();
+
+        novaTemp =     (unidade.equals("C")) ? (temp -32) * 5 / 9 : (temp * 9/5) + 32;
+
+        System.out.printf("%.1f° %s", novaTemp, unidade);
+
+        scanner.close();
 
     }
 }
